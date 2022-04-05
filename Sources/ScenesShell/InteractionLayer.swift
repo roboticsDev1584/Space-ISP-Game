8f82f753526bc745f3cc187e88724db120ed312e
roboticsDev1584 <matthew.caldarola@student.allenisd.org>
2022-04-05 12:51:01 -0500

    set up interaction layer

--- Sources/ScenesShell/InteractionLayer.swift
+++ Sources/ScenesShell/InteractionLayer.swift
@@ -1,18 +1,11 @@
+import Foundation
 import Scenes
-
-  /*
-     This class is responsible for the interaction Layer.
-     Internally, it maintains the RenderableEntities for this layer.
-   */
-
+import Igis
 
 class InteractionLayer : Layer {
 
       init() {
-          // Using a meaningful name can be helpful for debugging
           super.init(name:"Interaction")
-
-          // We insert our RenderableEntities in the constructor
-
       }
+      
   }
