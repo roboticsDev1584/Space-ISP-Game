bb0260288bf95d11151c1a584dab92dcca4937ec
Emma Vongchanh <emma.vongchanh@student.allenisd.org>
2022-04-17 19:10:10 -0500

    Made screens for when players choose their color ship

--- Sources/ScenesShell/BackgroundLayer.swift
+++ Sources/ScenesShell/BackgroundLayer.swift
@@ -9,14 +9,15 @@ class BackgroundLayer : Layer {
     //conversion: 30 = 1 second
     let starBackground = StarBackground(waitStar:90,changeStar:90,waitRedGiant:90,changeRedGiant:90,waitSupernova:60,enlargeBlackHole:90,starTargetMultiplier:1.6,redGiantTargetMultiplier:3.0,blackHoleTargetMultiplier:10.0)
     let startingScreen = StartingScreen()
-
+    let player1 = Player1Choose()
+    let player2 = Player2Choose()
     init() {
         // Using a meaningful name can be helpful for debugging
         super.init(name:"Background")
         
         // We insert our RenderableEntities in the constructor
         
-        insert(entity:backgroundChoice, at:.back)
+        insert(entity:player2, at:.back)
     }
     var previousStarState = -1
 }
