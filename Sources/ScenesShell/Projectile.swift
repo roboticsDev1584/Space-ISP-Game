54b9f1ee42fff3119656f019f8ab2e6e16f3ff38
roboticsDev1584 <matthew.caldarola@student.allenisd.org>
2022-05-10 19:44:44 -0500

    Fixed bouncing, but life decrement is weird

--- Sources/ScenesShell/Projectile.swift
+++ Sources/ScenesShell/Projectile.swift
@@ -8,7 +8,8 @@ class Projectile : RenderableEntity {
     let projectileBody : FillStyle
     let projectileOutline : StrokeStyle
     let asteroidRects : [Rect]
-
+    var asteroidRectsSides : [Int] = []
+    
     let fireVelocity : Double
     var fireVelocityXSign = 1
     var fireVelocityYSign = 1
@@ -17,6 +18,7 @@ class Projectile : RenderableEntity {
     var lives1 = 0
     var lives2 = 0
     let radius = 6
+    var inAstr = false
 
     var ship1XPointer : UnsafeMutablePointer<Int>
     var ship2XPointer : UnsafeMutablePointer<Int>
@@ -36,6 +38,11 @@ class Projectile : RenderableEntity {
         self.degree = degree
         self.terminate = false
         self.asteroidRects = rects
+
+        //initialize asteroidRectsSides array
+        for index2 in 0 ..< asteroidRects.count {
+            asteroidRectsSides.append(0)
+        }
 
         //set up the pointers
         ship1XPointer = .init(&ship1X)
@@ -63,13 +70,13 @@ class Projectile : RenderableEntity {
         //set the sensitivity for hitting the other player
         let rangeVal = 10
 
-        if ((projectile.center.x >= (ship1XPointer.pointee - rangeVal) && projectile.center.x <= (ship1XPointer.pointee + rangeVal)) && (projectile.center.y >= (ship1YPointer.pointee - rangeVal) && projectile.center.y <= (ship1YPointer.pointee + rangeVal))) {
+        if (((projectile.center.x >= (ship1XPointer.pointee - rangeVal) && projectile.center.x <= (ship1XPointer.pointee + rangeVal)) && (projectile.center.y >= (ship1YPointer.pointee - rangeVal) && projectile.center.y <= (ship1YPointer.pointee + rangeVal))) && !terminate) {
             terminate = true
             //player 1 loses one life
             lives1 -= 1
         }
         //if projectile hits player 2
-        else if ((projectile.center.x >= (ship2XPointer.pointee - rangeVal) && projectile.center.x <= (ship2XPointer.pointee + rangeVal)) && (projectile.center.y >= (ship2YPointer.pointee - rangeVal) && projectile.center.y <= (ship2YPointer.pointee + rangeVal))) {
+        else if (((projectile.center.x >= (ship2XPointer.pointee - rangeVal) && projectile.center.x <= (ship2XPointer.pointee + rangeVal)) && (projectile.center.y >= (ship2YPointer.pointee - rangeVal) && projectile.center.y <= (ship2YPointer.pointee + rangeVal))) && !terminate) {
             terminate = true
             //player 2 loses one life
             lives2 -= 1
@@ -102,20 +109,40 @@ class Projectile : RenderableEntity {
         //handle projectile direction change
         for index in 0 ..< asteroidRects.count {
             let asteroid = asteroidRects[index]
-            /*
-            let rectangle = Rectangle(rect:asteroid, fillMode:.fillAndStroke)
-            let stroke = StrokeStyle(color:Color(.white))
-            canvas.render(stroke, rectangle)*/
-            let projectileRectangle = Rectangle(rect:projectileRect, fillMode:.fillAndStroke)
-            let containment = asteroid.containment(target:projectileRect)
-            //change direction if hitting an asteroid
-            if (!containment.intersection([.containedHorizontally]).isEmpty) {
-                fireVelocityXSign *= -1
-            }
-            if (!containment.intersection([.containedVertically]).isEmpty) {
-                fireVelocityYSign *= -1
+            let astrContainment = asteroid.containment(target:projectileRect)
+            let sideState = asteroidRectsSides[index]
+            if (!astrContainment.intersection([.containedFully]).isEmpty) {        
+                if ((sideState == 1) || (sideState == 3)) {
+                    fireVelocityXSign *= -1
+                }
+                if ((sideState == 2) || (sideState == 3)) {
+                    fireVelocityYSign *= -1
+                }
+                if (sideState == 0) { //the projectile was fired into the center of the asteroid
+                    terminate = true
+                }
             }
         }
+        //update the asteroidRectsSides array for asteroid containment relation
+        var tempAsteroidRectsSides : [Int] = []
+        for index in 0 ..< asteroidRects.count {
+            let asteroid = asteroidRects[index]
+            let astrContainment = asteroid.containment(target:projectileRect)
+            //mark the projectile's relation to the asteroid
+            if (!astrContainment.intersection([.beyondFully, .overlapsFully]).isEmpty) {
+                tempAsteroidRectsSides.append(3)
+            }
+            else if (!astrContainment.intersection([.beyondHorizontally, .overlapsHorizontally]).isEmpty) {
+                tempAsteroidRectsSides.append(1)
+            }
+            else if (!astrContainment.intersection([.beyondVertically, .overlapsVertically]).isEmpty) {
+                tempAsteroidRectsSides.append(2)
+            }
+            else { //the projectile must be fully contained in the asteroid, so use the previous value to determine what side it is coming from
+                tempAsteroidRectsSides.append(asteroidRectsSides[index])
+            }
+        }
+        asteroidRectsSides = tempAsteroidRectsSides
         
         if (!terminate) {
             canvas.render(projectileBody, projectileOutline, projectile)
