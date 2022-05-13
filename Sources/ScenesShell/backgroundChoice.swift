be076b2c21d24260262097bc96d3c47285954f8d
Emma Vongchanh <emma.vongchanh@student.allenisd.org>
2022-05-13 18:42:18 -0500

    IDK what I changed it said I need to commit

--- Sources/ScenesShell/backgroundChoice.swift
+++ Sources/ScenesShell/backgroundChoice.swift
@@ -31,27 +31,27 @@ class ChooseMap : RenderableEntity {
         }
         let fillStyle = FillStyle(color:Color(.deepskyblue))
         let words = Text(location:Point(x:canvasSize.center.x-330,y:100), text:"Choose your setting")
-        words.font = "80pt Callout"
+        words.font = "80pt Megrim"
         canvas.render(fillStyle, words)
 
         let words1 = Text(location:Point(x:150,y:200), text:"Each setting has unique features which effects the gameplay")
-        words1.font = "50pt Callout"
+        words1.font = "50pt Josefin Sans"
         canvas.render(words1)
 
         let words2 = Text(location:Point(x:canvasSize.center.x-450, y:300), text:"Saturn (Press f)")
-        words2.font = "45pt Callout"
+        words2.font = "45pt Josefin Sans"
         canvas.render(words2)
 
         let words3 = Text(location:Point(x:canvasSize.center.x-470, y:400), text:"Neptune: Ships freeze after 1 minute (Press n)")
-        words3.font = "45pt Callout"
+        words3.font = "45pt Josefin Sans"
         canvas.render(words3)
 
         let words4 = Text(location:Point(x:canvasSize.center.x-675, y:500), text:"Mercury: Ships melt after 1 minute and 30 seconds (Press m)")
-        words4.font = "45pt Callout"
+        words4.font = "45pt Josefin Sans"
         canvas.render(words4)
 
         let words5 = Text(location:Point(x:30, y:600), text:"Star: Players must defeat each other before star fully turns into black hole (Press y)")
-        words5.font = "34pt Callout"
+        words5.font = "45pt Callout"
         canvas.render(words5)
         }
 }
--- Sources/ScenesShell/player1Choice.swift
+++ Sources/ScenesShell/player1Choice.swift
@@ -31,27 +31,27 @@ class Player1Choose : RenderableEntity {
         }
         let fillStyle = FillStyle(color:Color(.white))
         let words = Text(location:Point(x:canvasSize.center.x-600,y:100), text:"Player 1 choose your ship color")
-        words.font = "80pt Callout"
+        words.font = "80pt megrim"
         canvas.render(fillStyle, words)
 
         let fill1 = FillStyle(color:Color(.red))
         let words1 = Text(location:Point(x:canvasSize.center.x-245,y:200), text:"Red (Press t)")
-        words1.font = "45pt Callout"
+        words1.font = "45pt megrim"
         canvas.render(fill1, words1)
 
         let fill2 = FillStyle(color:Color(.deepskyblue))
         let words2 = Text(location:Point(x:canvasSize.center.x-250, y:300), text:"Blue (Press o)")
-        words2.font = "45pt Callout"
+        words2.font = "45pt megrim"
         canvas.render(fill2, words2)
 
         let fill3 = FillStyle(color:Color(.lime))
         let words3 = Text(location:Point(x:canvasSize.center.x-255, y:400), text:"Green (Press u)")
-        words3.font = "45pt Callout"
+        words3.font = "45pt megrim"
         canvas.render(fill3, words3)
 
         let fill4 = FillStyle(color:Color(.yellow))
         let words4 = Text(location:Point(x:canvasSize.center.x-265, y:500), text:"Yellow (Press v)")
-        words4.font = "45pt Callout"
+        words4.font = "45pt megrim"
         canvas.render(fill4, words4)
         }
 }
